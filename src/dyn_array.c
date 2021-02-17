b835ec2bf7ddbf3ad7225d6eb212f83e20bafa3a
JP Arimaa <juhapekka.arimaa@gmail.com>
2021-02-17 21:28:22 +0200

    Update dyn_array

--- src/dyn_array.c
+++ src/dyn_array.c
@@ -2,23 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-void dyn_array_create(struct dyn_array* self, int element_size)
+struct dyn_array* dyn_array_create(size_t element_size)
 {
-    self->size = 0;
-    self->reserved = 10;
+    struct dyn_array* self = calloc(1, sizeof(struct dyn_array));
     self->element_size = element_size;
-    self->data = calloc(self->reserved, element_size);
+    return self;
 }
 
 void dyn_array_push_back(struct dyn_array* self, void* element)
 {
     if (self->size == self->reserved)
     {
-        self->reserved *= 2;
-        self->data = realloc(self->data, self->reserved);
-        // Todo: check if realloc fails
+        self->reserved = (self->reserved * 2) + 1;
+        self->data = realloc(self->data, self->reserved * self->element_size);
     }
-    const int offset = self->size * self->element_size;
+    const size_t offset = self->size * self->element_size;
     char* data = self->data;
     memcpy(&data[offset], element, self->element_size);
     ++self->size;
@@ -33,25 +31,16 @@ void dyn_array_pop_back(struct dyn_array* self)
     --self->size;
 }
 
-bool dyn_array_set(struct dyn_array* self, int index, void* element)
+void dyn_array_set(struct dyn_array* self, size_t index, void* element)
 {
-    if (index >= self->size)
-    {
-        return false;
-    }
-    const int offset = self->size * self->element_size;
+    const size_t offset = index * self->element_size;
     char* data = self->data;
     memcpy(&data[offset], element, self->element_size);
-    return true;
 }
 
-void* dyn_array_get(struct dyn_array* self, int index)
+void* dyn_array_get(struct dyn_array* self, size_t index)
 {
-    if (index >= self->size)
-    {
-        return NULL;
-    }
-    const int offset = index * self->element_size;
+    const size_t offset = index * self->element_size;
     char* data = self->data;
     return &data[offset];
 }
@@ -59,4 +48,5 @@ void* dyn_array_get(struct dyn_array* self, int index)
 void dyn_array_destroy(struct dyn_array* self)
 {
     free(self->data);
+    free(self);
 }
--- src/hello.c
+++ src/hello.c
@@ -10,26 +10,34 @@ struct my_struct
 
 int main()
 {
-    struct dyn_array my_array;
-    dyn_array_create(&my_array, sizeof(struct my_struct));
+    struct dyn_array* my_array = dyn_array_create(sizeof(struct my_struct));
 
     {
-        struct my_struct data;
-        data.a = 10;
-        data.b = 20;
-        data.c = 30;
-        dyn_array_push_back(&my_array, &data);
-        data.a = 42;
-        dyn_array_push_back(&my_array, &data);
+        struct my_struct tmp1;
+        tmp1.a = 10;
+        tmp1.b = 20;
+        tmp1.c = 30;
+        dyn_array_push_back(my_array, &tmp1);
+        tmp1.a = 42;
+        dyn_array_push_back(my_array, &tmp1);
+        struct my_struct tmp2 = {1, 2, 3};
+        for (int i = 0; i < 3; ++i)
+        {
+            dyn_array_push_back(my_array, &tmp2);
+        }
+        tmp1.a = 99;
+        dyn_array_set(my_array, 3, &tmp1);
     }
 
-    for (int i = 0; i < my_array.size; ++i)
+    dyn_array_pop_back(my_array);
+
+    for (size_t i = 0; i < my_array->size; ++i)
     {
-        struct my_struct* data = dyn_array_get(&my_array, i);
+        struct my_struct* data = dyn_array_get(my_array, i);
         printf("%d %d %d\n", data->a, data->b, data->c);
     }
 
-    dyn_array_destroy(&my_array);
+    dyn_array_destroy(my_array);
 
     return 0;
 }
